066b8c4136f17365e4ef80217e8d19f16c99d524
moneyl <8206401+Moneyl@users.noreply.github.com>
2023-01-22 11:17:49 -0500

    Re-enable main menu lock
    Still need to move modder tools to DevTools plugin so the base RSL2 install doesn't have them

--- RSL2/rsl2/hooks/ui/MainMenuHooks.h
+++ RSL2/rsl2/hooks/ui/MainMenuHooks.h
@@ -10,22 +10,22 @@
 #endif
 
 //void __cdecl main_menu_process() //0x00513770
-FunHook<void ()> main_menu_process_hook
+FunHook<void()> main_menu_process_hook
 {
     0x00513770,
     []()
     {
-        static u32 mainMenuFrames = 0; //Number of frames the main menu has been alive
-        if (mainMenuFrames > 100) //Wait 100 frames to ensure the main menu has been created by the game
-        {
-            //Hide invalid RSL main menu options (MP and WC). Done in a hook to ensure the main menu exists first.
-            //Previously was done in RSL2_PluginInit and it could sometimes fail if it was done too early.
-            TryHideInvalidMainMenuOptions();
-        }
-        else
-        {
-            mainMenuFrames++;
-        }
+        //static u32 mainMenuFrames = 0; //Number of frames the main menu has been alive
+        //if (mainMenuFrames > 100) //Wait 100 frames to ensure the main menu has been created by the game
+        //{
+        //    //Hide invalid RSL main menu options (MP and WC). Done in a hook to ensure the main menu exists first.
+        //    //Previously was done in RSL2_PluginInit and it could sometimes fail if it was done too early.
+        //    TryHideInvalidMainMenuOptions();
+        //}
+        //else
+        //{
+        //    mainMenuFrames++;
+        //}
 
         main_menu_process_hook.CallTarget();
     }
