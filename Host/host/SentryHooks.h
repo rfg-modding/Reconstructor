038a1972a83138bf83ea08527c5ff3c383463a2d
arrowsv <simplearrows@hotmail.com>
2025-07-12 14:22:26 +1000

    chore: update and make Sentry DSN public

--- Host/host/SentryHooks.h
+++ Host/host/SentryHooks.h
@@ -2,10 +2,10 @@
 #include "common/patching/FunHook.h"
 #include "common/string/String.h"
 #include "common/Typedefs.h"
-#include "SentrySecrets.h"
 #include "pugixml.hpp"
 #include "sentry.h"
 #include <filesystem>
+#define SENTRY_DSN "https://813b13910cb32921cb43c85a1350e294@o4509619634176000.ingest.us.sentry.io/4509619648593920"
 
 extern void CheckUserAnalyticsOptIn();
 extern void InstallSentryHooks();
